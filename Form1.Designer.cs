ea45e690003a93cbdba263f6533df868873428d9
khajiitvaper2017 <kanalgera@gmail.com>
2020-11-23 18:11:54 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace DiscordCustomRPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(339, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 46);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Hide";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Открыть эту хуйню";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Инициализация";
+            this.label3.TextChanged += new System.EventHandler(this.Label3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 131);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(420, 170);
+            this.MinimumSize = new System.Drawing.Size(420, 170);
+            this.Name = "Form1";
+            this.Text = "Discord RPC Custom";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiscordCustomRPC
+{
+    public partial class Form1 : Form
+    {
+        private readonly string ClientID = "779000218573864980";                                  // Your Client ID at https://discord.com/developers/applications
+
+        private Assets assets = new Assets()                                                        //your images at https://discord.com/developers/applications
+        {
+            LargeImageKey = "obeme",
+            LargeImageText = "чекушка, порнушка, засерушка",
+            SmallImageKey = "rabbit",
+            SmallImageText = "крол ебать"
+        };
+
+        private readonly string savedProperty = Environment.CurrentDirectory + @"\savedProperty.txt";
+        private string wintitle;
+
+        public DiscordRpcClient client;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 64;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        public void Initialization()
+        {
+            client = new DiscordRpcClient(ClientID);
+
+            client.OnReady += (sender, e) =>
+            {
+                label1.Text = $"Received Ready from user {e.User.Username}";
+            };
+
+            client.OnPresenceUpdate += (sender, e) =>
+            {
+                label2.Text = $"Received Update! {e.Presence}";
+            };
+
+            client.Initialize();
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = label3.Text,
+                State = textBox2.Text,
+
+                Assets = assets
+            });
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(savedProperty))
+                textBox2.Text = File.ReadAllText(savedProperty);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wintitle = GetActiveWindowTitle();
+            label1.Text = "";
+            label2.Text = "";
+            Initialization();
+            timer1.Start();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            SetPresence();
+        }
+
+        public void SetPresence()
+        {
+            if (client != null)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = label3.Text,
+                    State = textBox2.Text,
+                    Assets = assets
+                });
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+            client.Deinitialize();
+            Initialization();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            notifyIcon1.Visible = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = GetActiveWindowTitle();
+        }
+
+        private void Label3_TextChanged(object sender, EventArgs e)
+        {
+            if (wintitle != label3.Text)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = label3.Text,
+                    State = textBox2.Text,
+                    Assets = assets
+                });
+                wintitle = label3.Text;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            File.WriteAllText(savedProperty, textBox2.Text);
+        }
+    }
+}
